cec0be67e0ebac8ee7cb3c77b6a53f1dfc56a6d2
Manish Raj <raj.manish@live.com>
2022-03-31 12:33:35 -0400

    Added the screen shots for the terraform project

--- project2/Exercise_2/main.tf
+++ project2/Exercise_2/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "ASIAXRI6BIRLZ3PZPNXN"
-  secret_key = "g+spRcWzAUrLkxhpCWrfo9DSqytddpWvrO8oZZqB"
-  token      = "FwoGZXIvYXdzEE8aDJRVLQuZLo2+/fIjAiLVAbys8u1hl4wVPT42Qt/fH6+Ij7Klh2PaOQ9f94mQY3v1sdavTBGKov64MxJRa7Ss6hVlY/WHTyjP7g4zMbwgF8bwNszn1ZdgSCWI3J0eYNhVnp4sUhmaT9zrOpD3CKNI4i46co/fxjUGfQ5hHUOnISzbs8NYdH5K7oti9WN4zAKkM4Ov2KKSWyEMRzcbCqNr0Mnn1h1DGNgNorl2zNvI/roJcRLeqrqDgerblGGld+7VxummRwVQ3NKPORu2Dk4pHYePdVWfmoGBNmVEvPo9tZj1Gj1IZyivx4iSBjItuk3nCgRedysz6NflaQCWjOxKEwYWRMJ07wfvv5nlH3T8qcqw1jlxvBlJvhQD"
+  # access_key = "ASIAXRI6BIRLZ3PZPNXN"
+  # secret_key = "g+spRcWzAUrLkxhpCWrfo9DSqytddpWvrO8oZZqB"
+  # token      = "FwoGZXIvYXdzEE8aDJRVLQuZLo2+/fIjAiLVAbys8u1hl4wVPT42Qt/fH6+Ij7Klh2PaOQ9f94mQY3v1sdavTBGKov64MxJRa7Ss6hVlY/WHTyjP7g4zMbwgF8bwNszn1ZdgSCWI3J0eYNhVnp4sUhmaT9zrOpD3CKNI4i46co/fxjUGfQ5hHUOnISzbs8NYdH5K7oti9WN4zAKkM4Ov2KKSWyEMRzcbCqNr0Mnn1h1DGNgNorl2zNvI/roJcRLeqrqDgerblGGld+7VxummRwVQ3NKPORu2Dk4pHYePdVWfmoGBNmVEvPo9tZj1Gj1IZyivx4iSBjItuk3nCgRedysz6NflaQCWjOxKEwYWRMJ07wfvv5nlH3T8qcqw1jlxvBlJvhQD"
   region     = var.aws_region
 }
 
@@ -15,9 +15,9 @@ resource "aws_lambda_function" "greet_lambda" {
   function_name = "greet_lambda"
   filename = data.archive_file.lambda.output_path
   runtime = "python3.8"
-  handler = "greet_lambda.handler"
+  handler = "greet_lambda.lambda_handler"
 
-  role = aws_iam_role.lambda_exec_1.arn
+  role = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_cloudwatch_log_group" "greet_lambda" {
@@ -26,8 +26,8 @@ resource "aws_cloudwatch_log_group" "greet_lambda" {
   retention_in_days = 30
 }
 
-resource "aws_iam_role" "lambda_exec_1" {
-  name = "serverless_lambda_1"
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -43,6 +43,6 @@ resource "aws_iam_role" "lambda_exec_1" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec_1.name
+  role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
